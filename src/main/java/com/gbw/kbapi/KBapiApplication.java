981cfbe1e1638ae2791f7b8f041de24ed9db6284
pitkwiecen <pitkwiecien@gmail.com>
2024-02-07 09:30:11 +0100

    initial commit

---
+++ src/main/java/com/gbw/kbapi/KBapiApplication.java
@@ -0,0 +1,13 @@
+package com.gbw.kbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KBapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KBapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gbw/kbapi/KBapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbw.kbapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KBapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
